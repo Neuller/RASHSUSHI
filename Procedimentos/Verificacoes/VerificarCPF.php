18067a1ff2c491121bdf11918b41f431ccff9caf
neuller <neuller97@hotmail.com>
2020-10-01 14:26:58 -0300

    ATUALIZAÇÃO - VERIFICAÇÃO CPF

--- Procedimentos/Verificacoes/VerificarCPF.php
+++ Procedimentos/Verificacoes/VerificarCPF.php
@@ -2,19 +2,22 @@
 require_once "../../Classes/Conexao.php";
 
 if(isset($_POST['CPF'])){
-
     $cpfPostado = $_POST['CPF'];
 
-    $c = new conectar();
-    $conexao = $c->conexao();
-    $sql = "SELECT * FROM clientes WHERE CPF = '{$cpfPostado}'";
+    if($cpfPostado == "000.000.000-00"){
+        echo json_encode("CPF NAO CADASTRADO");
+    }else{
+        $c = new conectar();
+        $conexao = $c->conexao();
+        $sql = "SELECT * FROM clientes WHERE CPF = '{$cpfPostado}'";
 
-    $result = mysqli_query($conexao, $sql) or print mysql_error();
+        $result = mysqli_query($conexao, $sql) or print mysql_error();
 
-    if(mysqli_num_rows($result) > 0){
-        echo json_encode("CPF JA CADASTRADO");
-    }else{ 
-        echo json_encode("CPF NAO CADASTRADO"); 
+        if(mysqli_num_rows($result) > 0){
+            echo json_encode("CPF JA CADASTRADO");
+        }else{ 
+            echo json_encode("CPF NAO CADASTRADO"); 
+        }
     }
 }
 ?>
